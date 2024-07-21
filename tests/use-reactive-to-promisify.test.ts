2f6057dd03d5083feab01c67985300ddb062e4a4
Carl Chen <ccheartszzz@gmail.com>
2024-07-22 00:45:38 +0800

    feat: add unit test

--- tests/use-reactive-to-promisify.test.ts
+++ tests/use-reactive-to-promisify.test.ts
@@ -5,7 +5,7 @@ import { ref, nextTick } from "vue";
 
 describe('reactive to promisify', () => {
 
-  test('convert the value of reactive to a promise', async () => {
+  test('convert the value of reactive to a return promise function', async () => {
     const mockFn = () => {
       const target = ref(false)
       setTimeout(() => {
@@ -27,7 +27,7 @@ describe('reactive to promisify', () => {
     expect(ret).resolves.toBe(true)
   })
 
-  test('Cache hook conversion', () => {
+  test('function calls result in independent', () => {
     const target = ref(false)
     const spyFn = vi.fn()
 
@@ -68,6 +68,29 @@ describe('reactive to promisify', () => {
   })
 
   test('custom watcher callback', () => {
-    // TODO:
+    const target = ref(false)
+
+    const mockFn = () => {
+      const mockData = {
+        foo: 'baz'
+      }
+
+      setTimeout(() => {
+        target.value = true
+      }, 2000)
+
+      return { mockData, target, onWatcherCallback: () => target.value }
+    }
+
+    vi.useFakeTimers()
+    const promisifyFn = useReactiveToPromisify(mockFn, (resolve, _, ret) => {
+      if (ret.target.value) {
+        resolve(ret.mockData)
+      }
+    })
+
+    const ret = promisifyFn()
+    vi.runAllTimers()
+    expect(ret).resolves.toEqual({ foo: 'baz' })
   })
 })
