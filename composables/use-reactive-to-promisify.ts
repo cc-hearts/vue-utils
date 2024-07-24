6931ae6edd62964ddd7e9f2681a0b7dbbad51dd6
Carl Chen <71313168+cc-hearts@users.noreply.github.com>
2024-07-24 21:24:41 +0800

    test: add useReactiveToPromisify unit test (#1)
    * wip: watcher
    
    * feat: add unit test
    
    * fix: fix bug
    
    * perf

--- composables/use-reactive-to-promisify.ts
+++ composables/use-reactive-to-promisify.ts
@@ -3,28 +3,32 @@ import { effectScope, watch } from 'vue'
 
 export function useReactiveToPromisify<
   T extends (...args: any) => any,
-  S = ReturnType<T>,
+  S = ReturnType<T> & { onWatcherCallback?: Fn },
   P = any
 >(hookFn: T, callback: (resolve: Fn, reject: Fn, ret: S) => void) {
-  const scope = effectScope()
-
   return () => {
-    const ret = new Promise<P>((resolve, reject) => {
-      scope.run(() => {
-        const ret = hookFn()
-        watch(
-          ret,
-          () => {
-            callback(resolve, reject, ret)
-          },
-          { immediate: true },
-        )
-      })
-    })
+      const scope = effectScope()
 
-    ret.finally(() => {
-      scope.stop()
-    })
+      const ret = new Promise<P>((resolve, reject) => {
+        scope.run(() => {
+          const ret = hookFn()
+          const watcher = ret?.onWatcherCallback || ret
+
+          watch(
+            watcher,
+            () => {
+              callback(resolve, reject, ret)
+            },
+            { immediate: true },
+          )
+        })
+      })
+
+      ret.finally(() => {
+        scope.stop()
+      }).catch(() => {
+        //
+      })
 
     return ret
   }
--- tests/use-reactive-to-promisify.test.ts
+++ tests/use-reactive-to-promisify.test.ts
@@ -5,7 +5,7 @@ import { ref, nextTick } from "vue";
 
 describe('reactive to promisify', () => {
 
-  test('convert the value of reactive to a promise', async () => {
+  test('convert the value of reactive to a return promise function', async () => {
     const mockFn = () => {
       const target = ref(false)
       setTimeout(() => {
@@ -27,7 +27,7 @@ describe('reactive to promisify', () => {
     expect(ret).resolves.toBe(true)
   })
 
-  test('Cache hook conversion', () => {
+  test('function calls result in independent', () => {
     const target = ref(false)
     const spyFn = vi.fn()
 
@@ -64,6 +64,62 @@ describe('reactive to promisify', () => {
 
     Promise.all(fns).then(() => {
       expect(spyFn).toHaveBeenCalledTimes(3)
-    }).catch(() => { })
+    }).catch(() => {/** */})
+  })
+
+  test('custom watcher callback', () => {
+    const target = ref(false)
+
+    const mockFn = () => {
+      const mockData = {
+        foo: 'baz'
+      }
+
+      setTimeout(() => {
+        target.value = true
+      }, 2000)
+
+      return { mockData, target, onWatcherCallback: () => target.value }
+    }
+
+    vi.useFakeTimers()
+    const promisifyFn = useReactiveToPromisify(mockFn, (resolve, _, ret) => {
+      if (ret.target.value) {
+        resolve(ret.mockData)
+      }
+    })
+
+    const ret = promisifyFn()
+    vi.runAllTimers()
+    expect(ret).resolves.toEqual({ foo: 'baz' })
+  })
+
+
+  // 如果callback抛出异常
+  test('callback throws exception', () => {
+    const target = ref(false)
+
+    const mockFn = () => {
+      const mockData = {
+        foo: 'baz'
+      }
+
+      setTimeout(() => {
+        target.value = true
+      }, 2000)
+
+      return { mockData, target, onWatcherCallback: () => target.value }
+    }
+
+    vi.useFakeTimers()
+    const promisifyFn = useReactiveToPromisify(mockFn, (_, reject, ret) => {
+      if (ret.target.value) {
+        reject(ret.mockData)
+      }
+    })
+
+    const ret = promisifyFn()
+    vi.runAllTimers()
+    expect(ret).rejects.toEqual({ foo: 'baz' })
   })
 })
